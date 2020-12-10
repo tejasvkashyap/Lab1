5ed36741c4f7c783842677e64ab76417c13e3561
tejasvkashyap <69615231+tejasvkashyap@users.noreply.github.com>
2020-12-10 11:03:05 +0530

    Update stud.java

--- stud.java
+++ stud.java
@@ -2,16 +2,56 @@
 import java.util.*;
 import java.io.*;
 import java.io.InputStreamReader;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class stud implements regi{
+public class stud<String> implements regi<String> {
 
+     //  private static JLabel label;
+     //  private static JTextField nam;
+     //  private static JButton btn;
+     //  private static JLabel done;
+      // private String name;
        Scanner get = new Scanner(System.in);
        public stud(){
+            
        }
        public String newstd(){
             System.out.println("Enter the new student name");
             String name = get.nextLine();
+           // JPanel panel = new JPanel();
+           // JFrame frame = new JFrame();
+           // frame.setSize(400,100);
+           // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+           // frame.add(panel);
+            
+          //  label = new JLabel("Enter the new student name:");
+           // label.setBounds(200,300,80,25);
+           // panel.add(label);
+
+
+        //    nam = new JTextField(10);
+         //   nam.setBounds(400,350,80,25);
+         //   panel.add(nam);
+            
+
+          //  btn = new JButton("Submit");
+         //   btn.setBounds(400,400,80,40);
+         //   btn.addActionListener(new stud()<String>);
+         //   panel.add(btn);
+
+         //   done = new JLabel();
+          //  done.setBounds(450,400,80,25);
+          //  panel.add(done);
+
+           // frame.setVisible(true);
             return name;
        }
+
+       //public void actionPerformed(ActionEvent e){
+       //     name = nam.getText();
+       //     done.setText("done");
+      // }
 
 }
