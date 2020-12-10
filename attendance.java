2de2767be6ffde1cd55ae501ce523bb697dcb8e7
tejasvkashyap <69615231+tejasvkashyap@users.noreply.github.com>
2020-12-10 11:04:11 +0530

    Update attendance.java

--- attendance.java
+++ attendance.java
@@ -8,19 +8,22 @@ import packages.*;
 class attendance
 {
          public static void main(String[] args) throws IOException {
+            String  sub;
+            Scanner get = new Scanner(System.in);
             strings welc = new strings();
             welc.stringbuffer();
-
+            System .out.println("Enter the subject name");
+            sub = get.nextLine();
             //command-line argument
             if(args.length>0){
                 thr t = new thr(); 
                 t.start();
-	        attend Obj = new attend(args[0]);
+	        attend <String> Obj = new attend<String>(sub,args[0]);
             }
             else{
                 Thread r = new Thread(new runa()); 
                 r.start();
-		attend Obj = new attend();
+		attend <String> Obj = new attend<String>(sub);
             }  
            welc.stri();
          }
